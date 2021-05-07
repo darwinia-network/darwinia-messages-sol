df4efc1fcd1f41529124729f866e11253c929792
xiaocheng <xiaoch2010@gmail.com>
2021-05-07 15:06:48 +0800

    add bytes32 option

---
+++ contracts/interfaces/IERC20Bytes32Option.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+interface IERC20Bytes32Option {
+    function name() external view returns (bytes32);
+    function symbol() external view returns (bytes32);
+}
+
