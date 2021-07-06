4ceca8de8c22d1f70736a2b5c116acd3ac719f36
xiaoch05 <xiaoch2010@gmail.com>
2021-07-06 16:06:21 +0800

    mapping token factory (#44)
    * add mapping token
    
    * add spec version & token type
    
    * dispatch call
    
    * transfer before burn
    
    * rename
    
    * add fee
    
    * add weight

--- contracts/mapping-token/contracts/interfaces/IERC20.sol
+++ contracts/mapping-token/contracts/interfaces/IERC20.sol
@@ -6,6 +6,7 @@ interface IERC20 {
   function totalSupply() external view returns (uint256);
   function balanceOf(address _who) external view returns (uint256);
   function transfer(address _to, uint256 _value) external returns (bool);
+  function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
   function mint(address _to, uint256 _value) external;
   function burn(address _from, uint256 _value) external;
   event Transfer(address indexed from, address indexed to, uint256 value);
