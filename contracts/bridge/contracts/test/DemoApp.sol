a99b521c155e408d926d7a2a1d1fdadeb2b2fe97
echo <hujw77@gmail.com>
2021-08-17 16:47:40 +0800

    simple app demo

--- contracts/bridge/contracts/test/DemoApp.sol
+++ contracts/bridge/contracts/test/DemoApp.sol
@@ -5,13 +5,6 @@ pragma solidity >=0.6.0 <0.7.0;
 import "../interfaces/ICrossChainFilter.sol";
 
 contract DemoApp is ICrossChainFilter {
-    event Log(address from, address to, uint256 amount);
-
-    address inbound;
-
-    constructor(address _inbound) public {
-        inbound = _inbound;
-    }
 
     function crossChainFilter(address sourceAccount, bytes memory) public override view returns (bool) {
         require(sourceAccount == address(0), "invalid source account");
