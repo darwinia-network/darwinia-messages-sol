0cbc058cbe632f27d53c63b4090f17e259143339
echo <hujw77@gmail.com>
2021-11-11 17:32:58 +0800

    aync test for message relay

--- contracts/bridge/test/v2/test_async_message_layer.js
+++ contracts/bridge/test/v2/test_async_message_layer.js
@@ -7,8 +7,9 @@ const log = console.log
 const thisChainPos = 0
 const bridgedChainPos = 1
 const lanePos = 0
-let outbound
-let inbound
+let outbound, inbound
+let outboundData, inboundData, a, b, c, d
+
 const send_message = async (nonce) => {
     const tx = await outbound.send_message(
       "0x0000000000000000000000000000000000000000",
@@ -26,49 +27,47 @@ const logNonce = async () => {
   log(`(${out.latest_received_nonce}, ${out.latest_generated_nonce}]                                            ->     (${iin.last_confirmed_nonce}, ${iin.last_delivered_nonce}]`)
 }
 
-const receive_messages_proof = async (nonce, flag = true) => {
-    outboundData = await outbound.data()
-    const tx = await inbound.receive_messages_proof(outboundData, "0x0000000000000000000000000000000000000000000000000000000000000000", "0x")
-    if (flag) {
+const receive_messages_proof = async (laneData, nonce) => {
+    const tx = await inbound.receive_messages_proof(laneData, "0x0000000000000000000000000000000000000000000000000000000000000000", "0x")
+    const n = await inbound.inboundLaneNonce()
+    const size = n.last_delivered_nonce - nonce
+    for (let i = 0; i<size; i++) {
       await expect(tx)
         .to.emit(inbound, "MessageDispatched")
-        .withArgs(bridgedChainPos, lanePos, nonce, false, "0x4c616e653a204d65737361676543616c6c52656a6563746564")
+        .withArgs(bridgedChainPos, lanePos, nonce+i, false, "0x4c616e653a204d65737361676543616c6c52656a6563746564")
     }
     await logNonce()
 }
 
-const receive_messages_delivery_proof = async (begin, end) => {
-    inboundData = await inbound.data()
-    const tx = await outbound.receive_messages_delivery_proof("0x0000000000000000000000000000000000000000000000000000000000000000", inboundData, "0x")
+const receive_messages_delivery_proof = async (laneData, begin, end) => {
+    const tx = await outbound.receive_messages_delivery_proof("0x0000000000000000000000000000000000000000000000000000000000000000", laneData, "0x")
     await expect(tx)
       .to.emit(outbound, "MessagesDelivered")
       .withArgs(bridgedChainPos, lanePos, begin, end, 0)
     await logNonce()
 }
 
-// out bound lane                                    ->           in bound lane
-// (latest_received_nonce, latest_generated_nonce]   ->     (last_confirmed_nonce, last_delivered_nonce]
-// (0, 1]   #send_message                            ->     (0, 0]
-// (0, 1]                                            ->     (0, 1]  #receive_messages_proof
-// (1, 1]   #receive_messages_delivery_proof         ->     (0, 1]
-// (1, 1]                                            ->     (1, 1]  #receive_messages_proof                       // here, new_confirmed_nonce = last_delivered_nonce
-// -----------------------------------------------------------------------------------------------------
-// (1, 2]   #send_message                            ->     (1, 1]
-// (1, 2]                                            ->     (1, 2]  #receive_messages_proof.a
-// (1, 3]   #send_message                            ->     (1, 2]
-// (1, 3]                                            ->     (1, 3]  #receive_messages_proof.b
-// (1, 4]   #send_message                            ->     (1, 3]
-// (2, 4]   #receive_messages_delivery_proof.a       ->     (1, 3]
-// (2, 4]                                            ->     (2, 3]  #receive_messages_proof.receive_state_update  // here, new_confirmed_nonce < last_delivered_nonce
-//                                                   ->     (2, 4]  #receive_messages_proof.receive_message.c
-// (3, 4]   #receive_messages_delivery_proof.b       ->     (2, 4]
-// (3, 4]                                            ->     (3, 4]  #receive_messages_proof.receive_state_update  // here, new_confirmed_nonce < last_delivered_nonce
-//                                                   ->     (3, 4]  #receive_messages_proof.receive_message.d
-// (3|4, 4] #receive_messages_delivery_proof.c|d     ->     (3, 4]
-// (4, 4]                                            ->     (4, 4]  #receive_messages_proof
+//   out bound lane                                    ->           in bound lane
+//   (latest_received_nonce, latest_generated_nonce]   ->     (last_confirmed_nonce, last_delivered_nonce]
+//0  (0, 1]   #send_message                            ->     (0, 0]
+//1  (0, 1]                                            ->     (0, 1]  #receive_messages_proof
+//2  (1, 1]   #receive_messages_delivery_proof         ->     (0, 1]
+//3  (1, 1]                                            ->     (1, 1]  #receive_messages_proof                       // here, new_confirmed_nonce = last_delivered_nonce
+//   -----------------------------------------------------------------------------------------------------
+//4  (1, 2]   #send_message                            ->     (1, 1]
+//5  (1, 2]                                            ->     (1, 2]  #receive_messages_proof.a
+//6  (1, 3]   #send_message                            ->     (1, 2]
+//7  (1, 3]                                            ->     (1, 3]  #receive_messages_proof.b
+//8  (1, 4]   #send_message                            ->     (1, 3]
+//9  (2, 4]   #receive_messages_delivery_proof.a       ->     (1, 3]
+//10 (2, 4]                                            ->     (2, 3]  #receive_messages_proof.receive_state_update  // here, new_confirmed_nonce < last_delivered_nonce
+//                                                     ->     (2, 4]  #receive_messages_proof.receive_message.c
+//11 (3, 4]   #receive_messages_delivery_proof.b       ->     (2, 4]
+//12 (3, 4]                                            ->     (3, 4]  #receive_messages_proof.receive_state_update  // here, new_confirmed_nonce < last_delivered_nonce
+//                                                     ->     (3, 4]  #receive_messages_proof.receive_message.d
+//13 (4, 4]   #receive_messages_delivery_proof.c|d     ->     (3, 4]
+//14 (4, 4]                                            ->     (4, 4]  #receive_messages_proof
 describe("async message relay tests", () => {
-  let outboundData
-  let inboundData
 
   before(async () => {
     const [owner] = await ethers.getSigners();
@@ -88,15 +87,18 @@ describe("async message relay tests", () => {
   });
 
   it("1", async function () {
-    await receive_messages_proof(1)
+    outboundData = await outbound.data()
+    await receive_messages_proof(outboundData, 1)
   });
 
   it("2", async function () {
-    await receive_messages_delivery_proof(1, 1)
+    inboundData = await inbound.data()
+    await receive_messages_delivery_proof(inboundData, 1, 1)
   });
 
   it("3", async function () {
-    await receive_messages_proof(1, false)
+    outboundData = await outbound.data()
+    await receive_messages_proof(outboundData, 1)
   });
 
   it("4", async function () {
@@ -104,7 +106,9 @@ describe("async message relay tests", () => {
   });
 
   it("5", async function () {
-    await receive_messages_proof(2)
+    outboundData = await outbound.data()
+    await receive_messages_proof(outboundData, 2)
+    a = await inbound.data()
   });
 
   it("6", async function () {
@@ -112,7 +116,42 @@ describe("async message relay tests", () => {
   });
 
   it("7", async function () {
-    log(JSON.stringify(outboundData, null, 2))
-    await receive_messages_proof(3)
+    outboundData = await outbound.data()
+    await receive_messages_proof(outboundData, 3)
+    b = await inbound.data()
+  });
+
+  it("8", async function () {
+    await send_message(4)
+  });
+
+  it("9", async function () {
+    await receive_messages_delivery_proof(a, 2, 2)
+  });
+
+  it("10", async function () {
+    outboundData = await outbound.data()
+    await receive_messages_proof(outboundData, 4)
+    c = await inbound.data()
+  });
+
+  it("11", async function () {
+    await receive_messages_delivery_proof(b, 3, 3)
+  });
+
+  it("12", async function () {
+    outboundData = await outbound.data()
+    await receive_messages_proof(outboundData, 4)
+    d = await inbound.data()
+  });
+
+  it("13", async function () {
+    await receive_messages_delivery_proof(c, 4, 4)
+    // await receive_messages_delivery_proof(d, 4, 4)
+  });
+
+  it("14", async function () {
+    outboundData = await outbound.data()
+    await receive_messages_proof(outboundData, 4)
   });
 });
