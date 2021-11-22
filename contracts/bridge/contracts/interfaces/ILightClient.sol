0484797dab84557f77da695189a52bea9a48dfac
echo <hujw77@gmail.com>
2021-11-22 19:37:13 +0800

    rename light client interface

--- contracts/bridge/contracts/interfaces/ILightClient.sol
+++ contracts/bridge/contracts/interfaces/ILightClient.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.6.0 <0.7.0;
 
 interface ILightClient {
     function verify_lane_data_proof(
-        bytes32 laneDataHash,
+        bytes32 lane_data_hash,
         uint32 chain_pos,
         uint32 lane_pos,
         bytes calldata proof
