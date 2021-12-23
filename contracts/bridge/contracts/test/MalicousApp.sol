75d948cc056833ce8a7600b67a021e6ec42ff878
echo <hujw77@gmail.com>
2021-12-23 16:26:05 +0800

    malicious app test

--- contracts/bridge/contracts/test/MalicousApp.sol
+++ contracts/bridge/contracts/test/MalicousApp.sol
@@ -3,14 +3,25 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 import "../interfaces/ICrossChainFilter.sol";
+import "../interfaces/IOnMessageDelivered.sol";
+import "../interfaces/IOutboundLane.sol";
 
-contract MalicousApp is ICrossChainFilter {
+contract MalicousApp is ICrossChainFilter, IOnMessageDelivered {
 
     function crossChainFilter(uint32, uint32, address, bytes calldata) external override view returns (bool) {
         return true;
     }
 
-    function loop(bytes calldata) external pure {
+    function malicious(address outlane, bytes memory large) public payable {
+        bytes memory encoded = abi.encode(this.loop.selector, large);
+        IOutboundLane(outlane).send_message{value: msg.value}(address(this), encoded);
+    }
+
+    function on_messages_delivered(uint256, bool) override external {
+        loop("");
+    }
+
+    function loop(bytes memory) public pure {
         uint cnt;
         while(true) {
             cnt++;
