3330add5dc726a1e3d5b3f6011292d2053d6d9e1
echo <hujw77@gmail.com>
2022-01-05 23:06:08 +0800

    support multi operation

--- packages/subgraph/TimeLock/src/mapping.ts
+++ packages/subgraph/TimeLock/src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { concat } from "@graphprotocol/graph-ts/helper-functions"
 import {
   TimeLock,
   CallExecuted,
@@ -9,42 +9,45 @@ import {
   RoleGranted,
   RoleRevoked
 } from "../generated/TimeLock/TimeLock"
-import { ProposalItem } from "../generated/schema"
+import { Proposal, Operation } from "../generated/schema"
 
 export function handleCallExecuted(event: CallExecuted): void {
-  let entity = ProposalItem.load(event.params.id.toHex())
-    if (entity == null) {
-      return
-    }
-  entity.status = 2
+  let entity = Proposal.load(event.params.id.toHex())
+  if (entity == null) {
+    return
+  }
+  entity.status = 'Executed'
   entity.save()
 }
 
 export function handleCallScheduled(event: CallScheduled): void {
-    let entity = new ProposalItem(event.params.id.toHex())
-    if (entity == null) {
-      return
-    }
-    entity.index = event.params.index
-    entity.target = event.params.target
-    entity.value = event.params.value
-    entity.data = event.params.data
-    entity.predecessor = event.params.predecessor
-    entity.delay = event.params.delay
-    entity.timestamp = event.block.timestamp
-  // 0 --- pending
-  // 1 --- ready
-  // 2 --- done
-  // 3 --- cancel
-    entity.status = 0
-    entity.save()
+  let proposal = Proposal.load(event.params.id.toHex())
+  if (proposal == null) {
+    proposal = new Proposal(event.params.id.toHex())
+    proposal.predecessor = event.params.predecessor
+    proposal.delay = event.params.delay
+    proposal.timestamp = event.block.timestamp
+    proposal.status = 'Pending'
+    proposal.save()
+  }
+
+  let id = event.params.id.toHex()
+  let index = event.params.index.toHex()
+  let op_id = id + index
+  let operation = new Operation(op_id)
+  operation.index = event.params.index
+  operation.target = event.params.target
+  operation.value = event.params.value
+  operation.data = event.params.data
+  operation.proposal = proposal.id
+  operation.save()
 }
 
 export function handleCancelled(event: Cancelled): void {
-  let entity = ProposalItem.load(event.params.id.toHex())
-    if (entity == null) {
-      return
-    }
-  entity.status = 3
+  let entity = Proposal.load(event.params.id.toHex())
+  if (entity == null) {
+    return
+  }
+  entity.status = 'Cancelled'
   entity.save()
 }
