828810cd5c9944577424dc80d4294f6fceadf393
xiaocheng <xiaoch2010@gmail.com>
2022-01-06 09:54:24 +0800

    update interface

--- contracts/precompile/contracts/sub2sub.sol
+++ contracts/precompile/contracts/sub2sub.sol
@@ -43,5 +43,36 @@ interface SubToSubBridge {
     /// @param message the payload of the message
     /// @param fee the fee of the bridge, we can get this value from fee market
     function encode_send_message_dispatch_call(uint32 msg_pallet_id, bytes4 lane_id, bytes memory message, uint256 fee) external view returns(bytes memory);
+
+    /// @dev get the scale encoded stream of the `register_from_remote` dispatch call which defined in s2s/issuing pallet
+    /// @param spec_version the spec_version of the remote chain
+    /// @param weight the remote dispatch call's weight, here is `unlock_from_remote`'s weight
+    /// @param token_type the token type of the original token, Native/Erc20 or others
+    /// @param token the original token address
+    /// @param name the name of the original token
+    /// @param symbol the symbol of the original token
+    /// @param decimals the decimals of the original token
+    function encode_register_from_remote_dispatch_call(
+        uint32 spec_version,
+        uint64 weight,
+        uint32 token_type,
+        address token,
+        string memory name,
+        string memory symbol,
+        uint8 decimals) external view returns(bytes memory);
+
+    /// @dev get the scale encoded stream of the `issue_from_remote` dispatch call which defined in s2s/issuing pallet
+    /// @param spec_version the spec_version of the remote chain
+    /// @param weight the remote dispatch call's weight, here is `unlock_from_remote`'s weight
+    /// @param token the original token address
+    /// @param recipient the receiver of the mapped token issued
+    /// @param amount the amount of the mapped token issued
+    function encode_issue_from_remote_dispatch_call(
+        uint32 spec_version,
+        uint64 weight,
+        address token,
+        address recipient,
+        uint256 amount
+    ) external view returns(bytes memory);
 }
 
