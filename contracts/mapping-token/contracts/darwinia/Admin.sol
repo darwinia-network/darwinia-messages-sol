c91f1d65c5c5bf840e9281e73a91d251f786caa6
echo <hujw77@gmail.com>
2022-01-14 14:50:41 +0800

    split wormhole

--- contracts/mapping-token/contracts/darwinia/Admin.sol
+++
@@ -1,3 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-import "@zeppelin-solidity-4.4.0/contracts/proxy/transparent/ProxyAdmin.sol";
--- contracts/mapping-token/contracts/test/MockFeeMarket.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.6.0;
-
-contract MockFeeMarket {
-    function market_fee() external pure returns (uint256 fee) {
-        return 10 ether;
-    }
-}
