6b64be5f6771063e7b48f7b3fd9f9b17f98f2a09
echo <hujw77@gmail.com>
2022-02-07 12:58:56 +0800

    vix

--- contracts/bridge/shell.nix
+++ contracts/bridge/shell.nix
@@ -1,18 +1,15 @@
 let
-  pkgs = import (builtins.fetchGit rec {
-    name = "dapptools-${rev}";
-    url = "https://github.com/hujw77/dapptools";
-    rev = "2a8a979baf21f36572892368f196601a05043524";
-  }) {};
-
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.dapptools {};
 in
   pkgs.mkShell {
     src = null;
-    name = "darwinia-bridge";
+    name = "darwinia-bridge-sol";
     buildInputs = with pkgs; [
       pkgs.dapp
       pkgs.seth
       pkgs.go-ethereum-unlimited
       pkgs.hevm
+      pkgs.nodejs
     ];
   }
