372e384110b43006912c9a0865c0fdd374368279
echo <hujw77@gmail.com>
2022-03-03 17:19:30 +0800

    eliminate warning

--- contracts/bridge/src/test/mock/NormalApp.sol
+++ contracts/bridge/src/test/mock/NormalApp.sol
@@ -6,6 +6,8 @@ import "../../interfaces/ICrossChainFilter.sol";
 
 contract NormalApp is ICrossChainFilter {
 
+    receive() external payable {}
+
     fallback() external payable {}
 
     function crossChainFilter(uint32, uint32, address, bytes calldata) override external pure returns (bool) {
