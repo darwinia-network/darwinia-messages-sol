4c269624aa102044af6064e73fb5657b32fd2d9d
xiaoch05 <xiaoch2010@gmail.com>
2022-04-20 18:02:42 +0800

    clean wormhole (#152)

--- contracts/wormhole/contracts/mapping-token/interfaces/IGuard.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.8.10;
-
-interface IGuard {
-  function deposit(uint256 id, address token, address recipient, uint256 amount) external;
-}
-
--- contracts/wormhole/flat/README.md
+++
@@ -1,8 +0,0 @@
-# Dao Flatten Files
-
-## solc params
-```json
-{
-	"optimizer": {"enabled":true,"runs":200}
-}
-```
