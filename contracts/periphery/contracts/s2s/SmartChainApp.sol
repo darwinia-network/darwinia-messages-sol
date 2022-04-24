46c77dbc154562a63861ec30622f69e586cbad96
Aki <wuminzhe@gmail.com>
2022-04-24 18:14:10 +0800

    add SmartChainApp

---
+++ contracts/periphery/contracts/s2s/SmartChainApp.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.7.0;
+
+import "../bridge/src/interfaces/ICrossChainFilter.sol";
+import "../bridge/src/interfaces/IOnMessageDelivered.sol";
+
+import "../utils/contracts/Scale.types.sol";
+
+abstract contract SmartChainApp is ICrossChainFilter, IOnMessageDelivered {
+	address public constant DISPATCH_ENCODER = 0x0000000000000000000000000000000000000018;
+    address public constant DISPATCH = 0x0000000000000000000000000000000000000019;
+
+    receive() external payable {}
+
+    fallback() external {}
+
+    function crossChainFilter(uint32, uint32, address, bytes calldata) external pure virtual override returns (bool) {
+        return true;
+    }
+
+    function on_messages_delivered(uint256, bool) external pure virtual override {
+        return;
+    }
+
+    // move to solidity encode?
+    function sendMessage(uint32 palletIndex, bytes4 laneId, bytes calldata message) public payable returns (bool) {
+    	// the pricision in contract is 18, and in pallet is 9, transform the fee value
+        uint256 fee = msg.value/(10**9); 
+        bytes memory sendMessageCall = SubToSubBridge(DISPATCH_ENCODER).encode_send_message_dispatch_call(
+            palletIndex,
+            laneId,
+            message,
+            fee
+        );
+        
+        (bool success, ) = DISPATCH.call(sendMessageCall);
+        return success;
+    }
+    
+    // origin?
+    function buildMessage(uint32 specVersion, uint64 weight, bytes calldata callEncoded) public pure returns (bytes memory) {
+        Types.EnumItemWithAccountId memory origin = Types.EnumItemWithAccountId(
+            2, // index in enum
+            origin
+        );
+        Types.EnumItemWithNull memory dispatchFeePayment = Types.EnumItemWithNull(0);
+        Types.Message memory message = Types.Message(
+            specVersion,
+            weight,
+            origin,
+            dispatchFeePayment,
+            callEncoded
+        );
+        return Types.encodeMessage(message);
+    }
+}
---
+++ contracts/periphery/contracts/s2s/interfaces/ISubToSubBridge.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+/**
+ * @dev This is the interface of substrate<>substrate bridge precompile contract through which solidity contracts
+ * will interact with Substrate To Substrate Bridge. 
+ * 
+ * TIP: We use read only function to avoid update state by these interface. Users who want to update state can get
+ * the encoded dispatch call value and then use the dispatch precompile to execute a dispatch call
+ * 
+ * @author Darwinia Network Team
+ */
+interface ISubToSubBridge {
+    /// @dev get the sub<>sub outbound latest generated message id, this id combined by laneid and nonce. It's also
+    /// the last sent message's id.
+    /// @param laneid The lane id in which message sent. Use this params to distinguish different channel.
+    function outbound_latest_generated_nonce(bytes4 laneid) external view returns (uint64);
+
+    /// @dev get the sub<>sub inbound latest received message id, this id combined by laneid and nonce.
+    /// #param laneid The land id in which message sent.
+    function inbound_latest_received_nonce(bytes4 laneid) external view returns (uint64);
+
+    /// #dev get the scale encoded payload of the `unlock_from_remote` dispatch call which defined in s2s/issuing pallet
+    /// this payload satisfy the standard format of the sub<>sub message payload
+    /// @param spec_version the spec_version of the remote chain
+    /// @param weight the remote dispatch call's weight, here is `unlock_from_remote`'s weight
+    /// @param token_type the token type of the original token, Native/Erc20 or others
+    /// @param original_token the address of the original token
+    /// @param recipient the reciver of the remote unlock token
+    /// @param amount of the original token unlocked
+    function encode_unlock_from_remote_dispatch_call(
+        uint32 spec_version,
+        uint64 weight,
+        uint32 token_type,
+        address original_token,
+        bytes memory recipient,
+        uint256 amount) external view returns(bytes memory);
+
+    /// @dev get the scale encoded stream of the sub<>sub send message dispatch call. then you can use dispatch precompile to
+    /// call it to send sub<>sub message
+    /// @param msg_pallet_id the sub<>sub message bridge pallet index
+    /// @param lane_id the bridge's lane id
+    /// @param message the payload of the message
+    /// @param fee the fee of the bridge, we can get this value from fee market
+    function encode_send_message_dispatch_call(uint32 msg_pallet_id, bytes4 lane_id, bytes memory message, uint256 fee) external view returns(bytes memory);
+
+    /// @dev get the scale encoded stream of the `register_from_remote` dispatch call which defined in s2s/issuing pallet
+    /// @param spec_version the spec_version of the remote chain
+    /// @param weight the remote dispatch call's weight, here is `unlock_from_remote`'s weight
+    /// @param token_type the token type of the original token, Native/Erc20 or others
+    /// @param token the original token address
+    /// @param name the name of the original token
+    /// @param symbol the symbol of the original token
+    /// @param decimals the decimals of the original token
+    function encode_register_from_remote_dispatch_call(
+        uint32 spec_version,
+        uint64 weight,
+        uint32 token_type,
+        address token,
+        string memory name,
+        string memory symbol,
+        uint8 decimals) external view returns(bytes memory);
+
+    /// @dev get the scale encoded stream of the `issue_from_remote` dispatch call which defined in s2s/issuing pallet
+    /// @param spec_version the spec_version of the remote chain
+    /// @param weight the remote dispatch call's weight, here is `unlock_from_remote`'s weight
+    /// @param token the original token address
+    /// @param recipient the receiver of the mapped token issued
+    /// @param amount the amount of the mapped token issued
+    function encode_issue_from_remote_dispatch_call(
+        uint32 spec_version,
+        uint64 weight,
+        address token,
+        address recipient,
+        uint256 amount
+    ) external view returns(bytes memory);
+}
+
