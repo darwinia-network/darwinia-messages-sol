3f5a6fca889dbc2cbc273fa2b07004a4252d1eeb
Aki <wuminzhe@gmail.com>
2022-04-25 10:47:43 +0800

    test abi encode

--- contracts/utils/contracts/AccountId.t.sol
+++ contracts/utils/contracts/AccountId.t.sol
@@ -12,9 +12,7 @@ contract AccountIdTest is DSTest {
     function setUp() public {}
 
     function testFromAddress() public {
-        // pangoro call
         bytes32 r = AccountId.fromAddress(0x6D6F646C64612f6272696e670000000000000000);
-
         bytes32 e = bytes32(0x64766d3a000000000000006d6f646c64612f6272696e67000000000000000015);
         assertEq(r, e);
     }
