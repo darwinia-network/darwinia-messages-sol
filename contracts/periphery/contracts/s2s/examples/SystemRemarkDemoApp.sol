12f4920790c56ee4e1fcd8e3d148f1c6bb7b69b4
Aki <wuminzhe@gmail.com>
2022-04-26 15:04:27 +0800

    add demo app, system remark

---
+++ contracts/periphery/contracts/s2s/examples/SystemRemarkDemoApp.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.7.0;
+
+import "../SmartChainApp.sol";
+import "@darwinia/contracts-utils/contracts/Scale.types.sol";
+
+// contract SystemRemarkDemoApp is PangolinToPangoroApp {
+contract SystemRemarkDemoApp is SmartChainApp {
+	function doSystemRemark() public payable {
+
+
+		// 1. prepare the message
+        //    this can be extract to a method:
+        //        Types.Message message = Pangoro.buildSystemRemarkCallMessage(hex"12345678");
+		System.RemarkCall memory call = System.RemarkCall(
+            hex"0001",
+            hex"12345678"
+        );
+        bytes memory callEncoded = System.encodeRemarkCall(call);
+        Types.Message memory message = buildMessage(28080, 2654000000, callEncoded);
+
+
+
+        // 2. send the message
+        //    Pangolin.sendMessageToPangoro(message);
+        //    or
+        //    Pangolin.ToPangoro.sendMessage(message);
+        sendMessage(bytes2(0x2b03), 0, 200000000000000000000, message);
+        //sendMessageOld(43, 0, 200000000000000000000, message);
+
+
+	}
+}
