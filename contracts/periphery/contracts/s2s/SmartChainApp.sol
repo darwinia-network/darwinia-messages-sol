97f1a7bd1a1469b4e380f570995bf4a92ca3f8c5
Aki <wuminzhe@gmail.com>
2022-04-26 15:02:35 +0800

    add old send message using precompile

--- contracts/periphery/contracts/s2s/SmartChainApp.sol
+++ contracts/periphery/contracts/s2s/SmartChainApp.sol
@@ -7,29 +7,48 @@ import "@darwinia/contracts-utils/contracts/Scale.types.sol";
 import "@darwinia/contracts-utils/contracts/AccountId.sol";
 
 abstract contract SmartChainApp {
-	address public constant DISPATCH_ENCODER = 0x0000000000000000000000000000000000000018;
+    address public constant DISPATCH_ENCODER = 0x0000000000000000000000000000000000000018;
     address public constant DISPATCH = 0x0000000000000000000000000000000000000019;
 
     receive() external payable {}
 
     fallback() external {}
 
-    // move to solidity encode?
     // TODO: 
-    //   define channel constant from palletIndex and laneId
-    function sendMessage(uint32 palletIndex, bytes4 laneId, bytes calldata message) public payable returns (bool) {
+    //   define constant from palletIndex and laneId
+    function sendMessage(bytes2 callIndex, bytes4 laneId, uint256 fee, Types.Message memory message) internal {
     	// the pricision in contract is 18, and in pallet is 9, transform the fee value
-        uint256 fee = msg.value/(10**9); 
-        bytes memory sendMessageCall = ISubToSubBridge(DISPATCH_ENCODER).encode_send_message_dispatch_call(
+        uint256 feeInPallet = fee/(10**9); 
+        BridgeMessages.SendMessageCall memory sendMessageCall = 
+            BridgeMessages.SendMessageCall(
+                callIndex,
+                laneId,
+                message,
+                uint128(feeInPallet)
+            );
+
+        bytes memory sendMessageCallEncoded = abi.encode(
+            BridgeMessages.encodeSendMessageCall(sendMessageCall)
+        );
+        
+        (bool success, bytes memory result) = DISPATCH.call(sendMessageCallEncoded);
+    }
+    function sendMessageOld(uint32 palletIndex, bytes4 laneId, uint256 fee, bytes memory message) internal { 
+        // the pricision in contract is 18, and in pallet is 9, transform the fee value
+        uint256 feeInPallet = fee/(10**9); 
+        bytes memory data = abi.encodeWithSelector(
+            ISubToSubBridge.encode_send_message_dispatch_call.selector,
             palletIndex,
             laneId,
             message,
-            fee
+            uint128(feeInPallet)
         );
-        
-        (bool success, ) = DISPATCH.call(sendMessageCall);
-        return success;
+        (bool success, bytes memory result) = DISPATCH_ENCODER.staticcall(data);
+        if (success) {
+            (bool success2, bytes memory result2) = DISPATCH.call(result);
+        }
     }
+
     
     function buildMessage(uint32 specVersion, uint64 weight, bytes memory callEncoded) internal returns (Types.Message memory) {
         Types.EnumItemWithAccountId memory origin = Types.EnumItemWithAccountId(
