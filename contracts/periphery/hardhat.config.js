ac85019df397337207bccdc26dd4cc3e52fa4814
wuminzhe <wuminzhe@gmail.com>
2022-05-17 14:03:23 +0800

    add a flat task to hardhat config

--- contracts/periphery/hardhat.config.js
+++ contracts/periphery/hardhat.config.js
@@ -15,6 +15,20 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+task("flat", "Flattens and prints contracts and their dependencies (Resolves licenses)")
+  .addOptionalVariadicPositionalParam("files", "The files to flatten", undefined, types.inputFile)
+  .setAction(async ({ files }, hre) => {
+    let flattened = await hre.run("flatten:get-flattened-sources", { files });
+
+    // Remove every line started with "// SPDX-License-Identifier:"
+    flattened = flattened.replace(/SPDX-License-Identifier:/gm, "License-Identifier:");
+    flattened = `// SPDX-License-Identifier: MIXED\n\n${flattened}`;
+
+    // Remove every line started with "pragma experimental ABIEncoderV2;" except the first one
+    flattened = flattened.replace(/pragma experimental ABIEncoderV2;\n/gm, ((i) => (m) => (!i++ ? m : ""))(0));
+    console.log(flattened);
+  });
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
