e8475bcbb24db13258e69a5cb07a0861c286a7a0
echo <hujw77@gmail.com>
2022-05-26 16:36:14 +0800

    refactor bsc light client

--- contracts/bridge/bin/deploy/local-dvm-2.sh
+++ contracts/bridge/bin/deploy/local-dvm-2.sh
@@ -2,29 +2,29 @@
 
 set -eo pipefail
 
-export NETWORK_NAME=local-dvm-1
+export NETWORK_NAME=local-dvm-2
 export ETH_RPC_URL=${TEST_LOCAL_DVM_RPC:-http://192.168.2.100:9933}
 export ETH_FROM=${TEST_LOCAL_DVM_FROM:-0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b}
 
 # import the deployment helpers
 . $(dirname $0)/common.sh
 
-# darwinia to eth2.0 bridge config
+# darwinia to bsc bridge config
 this_chain_pos=0
 this_out_lane_pos=0
 this_in_lane_pos=1
-bridged_chain_pos=1
+bridged_chain_pos=2
 bridged_in_lane_pos=1
 bridged_out_lane_pos=0
 
 # fee market config
 FEEMARKET_VAULT=0x0000000000000000000000000000000000000000
-COLLATERAL_PERORDER=$(seth --to-wei 10 ether)
+COLLATERAL_PERORDER=$(seth --to-wei 1 ether)
 ASSIGNED_RELAYERS_NUMBER=3
 SLASH_TIME=86400
 RELAY_TIME=86400
-# 0.01 : 2000
-PRICE_RATIO=999990
+# 0.01 : 300
+PRICE_RATIO=999900
 
 FeeMarket=$(deploy FeeMarket \
   $FEEMARKET_VAULT \
@@ -33,31 +33,17 @@ FeeMarket=$(deploy FeeMarket \
   $SLASH_TIME $RELAY_TIME \
   $PRICE_RATIO)
 
-# becon light client config
-BLS_PRECOMPILE=0x000000000000000000000000000000000000001c
-SLOT=0
-PROPOSER_INDEX=0
-PARENT_ROOT=
-STATE_ROOT=
-BODY_ROOT=
-CURRENT_SYNC_COMMITTEE_HASH=
-NEXT_SYNC_COMMITTEE_HASH=
-
-BeaconLightClient=$(deploy BeaconLightClient \
-  $BLS_PRECOMPILE \
-  $SLOT \
-  $PROPOSER_INDEX \
-  $PARENT_ROOT \
-  $STATE_ROOT \
-  $BODY_ROOT \
-  $CURRENT_SYNC_COMMITTEE_HASH \
-  $NEXT_SYNC_COMMITTEE_HASH)
+BSCLightClient=$(deploy BSCLightClient \
+  $bridged_chain_pos \
+  $LANE_IDENTIFY_SLOT \
+  $LANE_NONCE_SLOT \
+  $LANE_MESSAGE_SLOT)
 
 ChainMessageCommitter=$(deploy ChainMessageCommitter $this_chain_pos)
 LaneMessageCommitter=$(deploy LaneMessageCommitter $this_chain_pos $bridged_chain_pos)
 
 OutboundLane=$(deploy OutboundLane \
-  $BeaconLightClient \
+  $BSCLightClient \
   $FeeMarket \
   $this_chain_pos \
   $this_out_lane_pos \
@@ -65,7 +51,7 @@ OutboundLane=$(deploy OutboundLane \
   $bridged_in_lane_pos 1 0 0)
 
 InboundLane=$(deploy InboundLane \
-  $BeaconLightClient \
+  $BSCLightClient \
   $this_chain_pos \
   $this_in_lane_pos \
   $bridged_chain_pos \
