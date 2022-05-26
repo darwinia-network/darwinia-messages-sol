0210ed6274933969ce2ac45d643ef90712006e11
echo <hujw77@gmail.com>
2022-05-26 10:32:57 +0800

    test cases

---
+++ contracts/bridge/src/test/truth/bsc/BSCLightClient.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+import "../../test.sol";
+
+contract BSCLightClientTest is DSTest {
+//  seth call 0x000000000000000000000000000000000000001a "state_root()" --rpc-url http://localhost:9933
+    function test_state_root() public {}
+}
