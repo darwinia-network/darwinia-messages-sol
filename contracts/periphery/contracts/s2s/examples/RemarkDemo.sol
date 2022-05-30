b4318a69870106f71f8d2604cb34d7f1fa3e9814
wuminzhe <wuminzhe@gmail.com>
2022-05-30 15:55:54 +0800

    use remark_with_event in the remark demo

--- contracts/periphery/contracts/s2s/examples/RemarkDemo.sol
+++ contracts/periphery/contracts/s2s/examples/RemarkDemo.sol
@@ -9,6 +9,8 @@ import "@darwinia/contracts-utils/contracts/AccountId.sol";
 
 // CrabSmartChain remote call remark of Darwinia
 contract RemarkDemo is SmartChainXApp, Ownable {
+    event OutputNonce(uint256 nonce);
+
     constructor() public {
         // Globle settings
         // The SmartChainXApp has default globle settings
@@ -72,14 +74,14 @@ contract RemarkDemo is SmartChainXApp, Ownable {
     function remark() public payable {
         // 1. prepare the call that will be executed on the target chain
         System.RemarkCall memory call = System.RemarkCall(
-            hex"0001",
+            hex"0009", // the call index of remark_with_event
             hex"12345678"
         );
         bytes memory callEncoded = System.encodeRemarkCall(call);
 
         // 2. send the message
         MessagePayload memory payload = MessagePayload(
-            1200, // spec version of target chain <----------- This may be changed, go to https://darwinia.subscan.io/runtime get the latest spec version
+            1210, // spec version of target chain <----------- This may be changed, go to https://darwinia.subscan.io/runtime get the latest spec version
             2654000000, // call weight
             callEncoded // call encoded bytes
         );
@@ -87,6 +89,7 @@ contract RemarkDemo is SmartChainXApp, Ownable {
             0, // bridge id, which is the mapping key of bridgeConfigs
             payload // message payload
         );
+        emit OutputNonce(nonce);
     }
 
     function onMessageDelivered(
