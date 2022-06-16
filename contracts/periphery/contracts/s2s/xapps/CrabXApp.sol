e1a32d5c7bc7e88d23a77eb8f01154f0d459612c
wuminzhe <wuminzhe@gmail.com>
2022-06-16 16:03:28 +0800

    Update crab bridges default config

--- contracts/periphery/contracts/s2s/xapps/CrabXApp.sol
+++ contracts/periphery/contracts/s2s/xapps/CrabXApp.sol
@@ -21,16 +21,16 @@ abstract contract CrabXApp is SmartChainXApp, Ownable {
 
     BridgeConfig internal toDarwinia =
         BridgeConfig(
-            // storage key for Darwinia market fee
             0xe0c938a0fbc88db6078b53e160c7c3ed2edb70953213f33a6ef6b8a5e3ffcab2,
-            // storage key for the latest nonce of Darwinia message lane
-            0xc9b76e645ba80b6ca47619d64cb5e58d96c246acb9b55077390e3ca723a0ca1f
+            0xf1501030816118b9129255f5096aa9b296c246acb9b55077390e3ca723a0ca1f
         );
 
     BridgeConfig internal toCrabParachain =
         BridgeConfig(
             0x190d00dd4103825c78f55e5b5dbf8bfe2edb70953213f33a6ef6b8a5e3ffcab2,
             0xc9b76e645ba80b6ca47619d64cb5e58d96c246acb9b55077390e3ca723a0ca1f
+            0x2158e364c657788d669f15db7687496b2edb70953213f33a6ef6b8a5e3ffcab2,
+            0xef3be8173575ddc682e1a72d92ce0b2696c246acb9b55077390e3ca723a0ca1f
         );
 
     // You need to consider providing set methods with permission control
