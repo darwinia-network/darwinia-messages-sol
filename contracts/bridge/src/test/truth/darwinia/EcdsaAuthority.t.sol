1d6894bc22aa758acce7e90ba9680b6ecf7135ad
echo <hujw77@gmail.com>
2022-07-29 15:29:53 +0800

    test add_relayer

--- contracts/bridge/src/test/truth/darwinia/EcdsaAuthority.t.sol
+++ contracts/bridge/src/test/truth/darwinia/EcdsaAuthority.t.sol
@@ -1,21 +1,89 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.7.6;
+pragma abicoder v2;
 
 import "../../test.sol";
+import "../../../utils/ECDSA.sol";
 import "../../../truth/darwinia/EcdsaAuthority.sol";
 
+interface Hevm {
+    function sign(uint256,bytes32) external returns (uint8,bytes32,bytes32);
+    function addr(uint256) external returns (address);
+}
+
 contract EcdsaAuthorityTest is DSTest {
+    uint constant sk = 1;
+    Hevm internal hevm = Hevm(HEVM_ADDRESS);
+    address private constant SENTINEL = address(0x1);
+    bytes4 private constant ADD_RELAYER_SIG = bytes4(0xb7aafe32);
+    bytes32 private constant RELAY_TYPEHASH = 0x30a82982a8d5050d1c83bbea574aea301a4d317840a8c4734a308ffaa6a63bc8;
+
     EcdsaAuthority authority;
+    address alice;
 
     function setUp() public {
         address[] memory relayers = new address[](1);
-        relayers[0] = 0x68898dB1012808808C903F390909C52D9F706749;
+        alice = hevm.addr(sk);
+        relayers[0] = alice;
         authority = new EcdsaAuthority(
             domain_separator(),
             relayers,
             1,
             0
         );
+    }
+
+    function test_add_relayer() public {
+        address bob = address(0xbb);
+        uint threshold = 1;
+        perform_add_relayer(bob, threshold);
+        address[] memory e = authority.get_relayers();
+        assertEq(e.length, 2);
+        assertEq(bob, e[0]);
+        assertEq(alice, e[1]);
+        assertEq(authority.get_threshold(), threshold);
+    }
+
+    function test_add_relayer_with_threshold() public {
+        address cici = address(0xc);
+        uint threshold = 2;
+        perform_add_relayer(cici, threshold);
+        address[] memory e = authority.get_relayers();
+        assertEq(e.length, 2);
+        assertEq(cici, e[0]);
+        assertEq(alice, e[1]);
+        assertEq(authority.get_threshold(), threshold);
+    }
+
+    function testFail_add_relayer_with_zero_threshold() public {
+        address cici = address(0xcc);
+        uint threshold = 0;
+        perform_add_relayer(cici, threshold);
+        address[] memory e = authority.get_relayers();
+    }
+
+    function testFail_add_relayer_with_wrong_threshold() public {
+        address cici = address(0xccc);
+        uint threshold = 3;
+        perform_add_relayer(cici, threshold);
+        address[] memory e = authority.get_relayers();
+    }
+
+    function perform_add_relayer(address x, uint threshold) public {
+        bytes32 struct_hash = keccak256(
+            abi.encode(
+                RELAY_TYPEHASH,
+                ADD_RELAYER_SIG,
+                abi.encode(x, threshold),
+                authority.nonce()
+            )
+        );
+        bytes32 digest = ECDSA.toTypedDataHash(domain_separator(), struct_hash);
+        (uint8 v, bytes32 r, bytes32 s) = hevm.sign(sk, digest);
+        bytes[] memory signs = new bytes[](1);
+        signs[0] = abi.encodePacked(r, s, v);
+
+        authority.add_relayer(x, threshold, signs);
     }
 
     function domain_separator() public pure returns (bytes32 s) {
