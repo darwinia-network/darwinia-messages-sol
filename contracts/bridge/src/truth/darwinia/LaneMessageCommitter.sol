6d432ef042ce647d58645ea4a598cfc3148b9c93
echo <hujw77@gmail.com>
2022-08-04 13:47:47 +0800

    add changeLane in LaneMessageCommitter

--- contracts/bridge/src/truth/darwinia/LaneMessageCommitter.sol
+++ contracts/bridge/src/truth/darwinia/LaneMessageCommitter.sol
@@ -3,25 +3,34 @@
 pragma solidity 0.7.6;
 pragma abicoder v2;
 
+import "../../utils/Math.sol";
 import "../../interfaces/IMessageCommitment.sol";
 
-contract LaneMessageCommitter {
+/// @title LaneMessageCommitter
+/// @notice Lane message committer commit all messages from this chain to bridged chain
+/// @dev Lane message use sparse merkle tree to commit all messages
+contract LaneMessageCommitter is Math {
     event Registry(uint256 outLanePos, address outboundLane, uint256 inLanePos, address inboundLane);
+    event ChangeLane(uint256 pos, address lane);
 
+    /// @dev This chain position of the leaf in the `chain_message_merkle_tree`, index starting with 0
     uint256 public immutable thisChainPosition;
+    /// @dev Bridged chain position of the leaf in the `chain_message_merkle_tree`, index starting with 0
     uint256 public immutable bridgedChainPosition;
-    uint256 public laneCount;
-    mapping(uint256 => address) public lanes;
-
+    /// @dev Count of all lanes in committer
+    uint256 public count;
+    /// lane positon => lane address
+    mapping(uint256 => address) public laneOf;
+    /// Governance role to registry new lane
     address public setter;
 
     modifier onlySetter {
-        require(msg.sender == setter, "Commit: forbidden");
+        require(msg.sender == setter, "forbidden");
         _;
     }
 
     constructor(uint256 _thisChainPosition, uint256 _bridgedChainPosition) {
-        require(_thisChainPosition != _bridgedChainPosition, "Commit: invalid position");
+        require(_thisChainPosition != _bridgedChainPosition, "!pos");
         thisChainPosition = _thisChainPosition;
         bridgedChainPosition = _bridgedChainPosition;
         setter = msg.sender;
@@ -31,25 +40,35 @@ contract LaneMessageCommitter {
         setter = _setter;
     }
 
+    function changeLane(uint256 pos, address lane) external onlySetter {
+        require(laneOf[pos] != address(0), "!exist");
+        (uint32 _thisChainPosition, uint32 _thisLanePosition, uint32 _bridgedChainPosition, ) = IMessageCommitment(lane).getLaneInfo();
+        require(thisChainPosition == _thisChainPosition, "!thisChainPosition");
+        require(bridgedChainPosition == _bridgedChainPosition, "!bridgedChainPosition");
+        require(pos == _thisLanePosition, "!thisLanePosition");
+        laneOf[pos] = lane;
+        emit ChangeLane(pos, lane);
+    }
+
     function registry(address outboundLane, address inboundLane) external onlySetter {
         (uint32 _thisChainPositionOut, uint32 _thisLanePositionOut, uint32 _bridgedChainPositionOut, ) = IMessageCommitment(outboundLane).getLaneInfo();
         (uint32 _thisChainPositionIn, uint32 _thisLanePositionIn, uint32 _bridgedChainPositionIn, ) = IMessageCommitment(inboundLane).getLaneInfo();
-        require(thisChainPosition == _thisChainPositionOut, "Commit: invalid ThisChainPosition");
-        require(thisChainPosition == _thisChainPositionIn, "Commit: invalid ThisChainPosition");
-        require(bridgedChainPosition == _bridgedChainPositionOut, "Commit: invalid chain position");
-        require(bridgedChainPosition == _bridgedChainPositionIn, "Commit: invalid chain position");
-        uint256 outLanePos = laneCount;
-        uint256 inLanePos = laneCount + 1;
-        require(outLanePos == _thisLanePositionOut, "Commit: invalid outlane position");
-        require(inLanePos == _thisLanePositionIn, "Commit: invalid inlane position");
-        lanes[outLanePos] = outboundLane;
-        lanes[inLanePos] = inboundLane;
-        laneCount += 2;
+        require(thisChainPosition == _thisChainPositionOut, "!thisChainPosition");
+        require(thisChainPosition == _thisChainPositionIn, "!thisChainPosition");
+        require(bridgedChainPosition == _bridgedChainPositionOut, "!bridgedChainPosition");
+        require(bridgedChainPosition == _bridgedChainPositionIn, "!bridgedChainPosition");
+        uint256 outLanePos = count;
+        uint256 inLanePos = count + 1;
+        require(outLanePos == _thisLanePositionOut, "!thisLanePosition");
+        require(inLanePos == _thisLanePositionIn, "!thisLanePosition");
+        laneOf[outLanePos] = outboundLane;
+        laneOf[inLanePos] = inboundLane;
+        count += 2;
         emit Registry(outLanePos, outboundLane, inLanePos, inboundLane);
     }
 
     function commitment(uint256 lanePos) public view returns (bytes32) {
-        address lane = lanes[lanePos];
+        address lane = laneOf[lanePos];
         if (lane == address(0)) {
             return bytes32(0);
         } else {
@@ -59,8 +78,8 @@ contract LaneMessageCommitter {
 
     // we use sparse tree to commit
     function commitment() public view returns (bytes32) {
-        bytes32[] memory hashes = new bytes32[](roundUpToPow2(laneCount));
-        for (uint256 pos = 0; pos < laneCount; pos++) {
+        bytes32[] memory hashes = new bytes32[](get_power_of_two_ceil(count));
+        for (uint256 pos = 0; pos < count; pos++) {
             hashes[pos] = commitment(pos);
         }
         uint256 hashLength = hashes.length;
@@ -72,11 +91,5 @@ contract LaneMessageCommitter {
             hashLength = hashLength - j;
         }
         return hashes[0];
-    }
-
-    // --- Math ---
-    function roundUpToPow2(uint256 len) internal pure returns (uint256) {
-        if (len <= 1) return 1;
-        else return 2 * roundUpToPow2((len + 1) / 2);
     }
 }
---
+++ contracts/bridge/src/utils/Math.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+contract Math {
+    /// Get the power of 2 for given input, or the closest higher power of 2 if the input is not a power of 2.
+    /// Commonly used for "how many nodes do I need for a bottom tree layer fitting x elements?"
+    /// Example: 0->1, 1->1, 2->2, 3->4, 4->4, 5->8, 6->8, 7->8, 8->8, 9->16.
+    function get_power_of_two_ceil(uint256 x) internal pure returns (uint256) {
+        if (x <= 1) return 1;
+        else if (x == 2) return 2;
+        else return 2 * get_power_of_two_ceil((x + 1) >> 1);
+    }
+}
