673ac5384327c361ac98fac7ffc581cb6645dd4f
echo <hujw77@gmail.com>
2022-08-16 10:21:13 +0800

    send all

--- contracts/claims/bin/send.sh
+++ contracts/claims/bin/send.sh
@@ -11,14 +11,16 @@ echo "path: $path"
 
 to=0xA561c8F6AC9eCb31e0793fBe2CCEc136a7e4bE84
 token=0xB29DA7C1b1514AB342afbE6AB915252Ad3f87E4d
-ids=$(jq -r ".[].erc721[].ids[0]" "$path")
+ids=$(jq -r ".[].erc721[].ids[]" "$path")
 
-nonce=$(seth nonce $ETH_FROM)
-echo "nonce: ${nonce}"
-for id in $ids;do
-  echo "sending token [$id] to [$to]"
-  seth send -F "$ETH_FROM" -N "$nonce" $token "transferFrom(address,address,uint)" "${ETH_FROM?}" "${to?}" "${id?}" --chain "${SETH_CHAIN?}"
-  nonce=$(( ${nonce} + 1 ))
-  echo "sent token [$id] to [$to]"
-done
-echo "nonce: ${nonce}"
+echo $ids
+
+# nonce=$(seth nonce $ETH_FROM)
+# echo "nonce: ${nonce}"
+# for id in $ids;do
+#   echo "sending token [$id] to [$to]"
+#   seth send -F "$ETH_FROM" -N "$nonce" $token "transferFrom(address,address,uint)" "${ETH_FROM?}" "${to?}" "${id?}" --chain "${SETH_CHAIN?}"
+#   nonce=$(( ${nonce} + 1 ))
+#   echo "sent token [$id] to [$to]"
+# done
+# echo "nonce: ${nonce}"
