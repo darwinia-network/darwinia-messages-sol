96adfc551403e90ab6629f813f3c6d502259e6e9
echo <hujw77@gmail.com>
2022-08-23 17:28:27 +0800

    fmt

--- contracts/bridge/src/proxy/truth/DarwiniaLightClientProxy.sol
+++ contracts/bridge/src/proxy/truth/DarwiniaLightClientProxy.sol
@@ -20,9 +20,9 @@ pragma solidity 0.7.6;
 import "../TransparentUpgradeableProxy.sol";
 
 contract DarwiniaLightClientProxy is TransparentUpgradeableProxy {
-	constructor(
-		address _logic,
-		address _admin,
-		bytes memory _data
-	) payable TransparentUpgradeableProxy(_logic, _admin, _data) {}
+    constructor(
+        address _logic,
+        address _admin,
+        bytes memory _data
+    ) payable TransparentUpgradeableProxy(_logic, _admin, _data) {}
 }
