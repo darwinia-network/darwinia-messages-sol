a142a9eefdeed8c690b10d0176d79e58776c7431
wuminzhe <wuminzhe@gmail.com>
2022-08-26 12:51:37 +0800

    Add ForwardToMoonbeamCall

--- contracts/periphery/contracts/s2s/types/PalletMessageRouter.t.sol
+++ contracts/periphery/contracts/s2s/types/PalletMessageRouter.t.sol
@@ -21,15 +21,16 @@ contract PalletMessageRouterTest is DSTest {
         // build transacts
         PalletMessageRouter.Transact[]
             memory transacts = new PalletMessageRouter.Transact[](1);
-        transacts[0] = PalletMessageRouter.Transact(
-            0,
-            12,
-            hex"1234"
-        );
+        transacts[0] = PalletMessageRouter.Transact(0, 12, hex"1234");
+        PalletMessageRouter.VersionedXcmV2WithTransacts
+            memory message = PalletMessageRouter.VersionedXcmV2WithTransacts(
+                transacts
+            );
 
         // encode VersionedXcm
-        bytes memory data = PalletMessageRouter.encodeVersionedXcmV2WithTransacts(transacts);
-        
+        bytes memory data = PalletMessageRouter
+            .encodeVersionedXcmV2WithTransacts(message);
+
         assertEq0(data, hex"0204060030081234");
     }
 }
