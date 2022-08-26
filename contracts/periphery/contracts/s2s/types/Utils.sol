b6a2e782f3666f31e6da221f3fff0965078ed1d4
wuminzhe <wuminzhe@gmail.com>
2022-08-26 09:33:42 +0800

    Remove unused imports

--- contracts/periphery/contracts/s2s/types/Utils.sol
+++ contracts/periphery/contracts/s2s/types/Utils.sol
@@ -2,10 +2,6 @@
 
 pragma solidity >=0.6.0;
 
-import "@darwinia/contracts-utils/contracts/Bytes.sol";
-import "@darwinia/contracts-utils/contracts/ScaleCodec.sol";
-import "hardhat/console.sol";
-
 library Utils {
     function encodeEnumItem(uint8 _enumItemIndex, bytes memory _enumItemData)
         internal
