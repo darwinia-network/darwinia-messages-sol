f837ed7a1e472c75a25b53d43b44b9e9f9b9a20a
wuminzhe <wuminzhe@gmail.com>
2022-08-29 20:11:46 +0800

    Add Instruction::DescendOrigin

--- contracts/periphery/contracts/s2s/types/PalletMessageRouter.t.sol
+++ contracts/periphery/contracts/s2s/types/PalletMessageRouter.t.sol
@@ -12,25 +12,50 @@ pragma experimental ABIEncoderV2;
 contract PalletMessageRouterTest is DSTest {
     function setUp() public {}
 
-    // message	            02 04
-    //                      06
-    // originType	        00
-    // requireWeightAtMost	30
-    // encoded call         08 1234
+    // {
+    //     V2: [
+    //         {
+    //         DescendOrigin: {
+    //             X1: {
+    //             AccountKey20: {
+    //                 network: {
+    //                 Named: CrabSmartChain
+    //                 }
+    //                 key: 0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac
+    //             }
+    //             }
+    //         }
+    //         }
+    //         {
+    //         Transact: {
+    //             originType: SovereignAccount
+    //             requireWeightAtMost: 5,000,000,000
+    //             call: {
+    //             encoded: 0x260000400d03000000000000000000000000000000000000000000000000000000000001004617d470f847ce166019d19a7944049ebb01740000000000000000000000000000000000000000000000000000000000000000001019ff1d2100
+    //             }
+    //         }
+    //         }
+    //     ]
+    // }
+    // callIndex	1a01
+    // message	02 08
+    // 0b 01 03
+    // network	01 38 43726162536d617274436861696e
+    // key	f24FF3a9CF04c71Dbc94D0b566f7A27B94566cac
+    // 06
+    // originType	01
+    // requireWeightAtMost	0700f2052a01
+    // encoded	7d01 260000400d03000000000000000000000000000000000000000000000000000000000001004617d470f847ce166019d19a7944049ebb01740000000000000000000000000000000000000000000000000000000000000000001019ff1d2100
     function testEncodeVersionedXcmV2() public {
-        // build transacts
-        PalletMessageRouter.Transact[]
-            memory transacts = new PalletMessageRouter.Transact[](1);
-        transacts[0] = PalletMessageRouter.Transact(0, 12, hex"1234");
-        PalletMessageRouter.VersionedXcmV2WithTransacts
-            memory message = PalletMessageRouter.VersionedXcmV2WithTransacts(
-                transacts
-            );
-
-        // encode VersionedXcm
-        bytes memory data = PalletMessageRouter
-            .encodeVersionedXcmV2WithTransacts(message);
-
-        assertEq0(data, hex"0204060030081234");
+        bytes memory call = PalletMessageRouter.buildForwardToMoonbeamCall(
+            0x1a01,
+            hex"43726162536d617274436861696e",
+            0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac,
+            hex"260000400d03000000000000000000000000000000000000000000000000000000000001004617d470f847ce166019d19a7944049ebb01740000000000000000000000000000000000000000000000000000000000000000001019ff1d2100"
+        );
+        assertEq0(
+            call,
+            hex"1a0102080b0103013843726162536d617274436861696ef24FF3a9CF04c71Dbc94D0b566f7A27B94566cac06010700f2052a017d01260000400d03000000000000000000000000000000000000000000000000000000000001004617d470f847ce166019d19a7944049ebb01740000000000000000000000000000000000000000000000000000000000000000001019ff1d2100"
+        );
     }
 }
