e97786312844c08856168b90334756b61954a381
echo <hujw77@gmail.com>
2022-08-30 20:06:24 +0800

    use alchemy api

--- contracts/bridge/e2e/message_storage_proof.js
+++ contracts/bridge/e2e/message_storage_proof.js
@@ -149,7 +149,7 @@ describe("bridge e2e test: verify message/storage proof", () => {
     }
   })
 
-  it.skip("3", async function () {
+  it("3", async function () {
     await bridge.relay_sub_header()
   })
 
