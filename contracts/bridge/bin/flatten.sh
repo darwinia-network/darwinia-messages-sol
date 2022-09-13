adcb97791bc44e961614a60c49d7259cd7e1f63e
echo <hujw77@gmail.com>
2022-09-13 15:35:15 +0800

    pre request audit

--- contracts/bridge/bin/flatten.sh
+++ contracts/bridge/bin/flatten.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-
-set -x
+rm -rf flat/
+mkdir -p flat
 
 hevm flatten --source-file src/fee-market/FeeMarket.sol                    --json-file out/dapp.sol.json > flat/FeeMarket.f.sol
 hevm flatten --source-file src/fee-market/SimpleFeeMarket.sol              --json-file out/dapp.sol.json > flat/SimpleFeeMarket.f.sol
 hevm flatten --source-file src/message/InboundLane.sol                     --json-file out/dapp.sol.json > flat/InboundLane.f.sol
 hevm flatten --source-file src/message/OutboundLane.sol                    --json-file out/dapp.sol.json > flat/OutboundLane.f.sol
 hevm flatten --source-file src/proxy/fee-market/FeeMarketProxy.sol         --json-file out/dapp.sol.json > flat/FeeMarketProxy.f.sol
-# hevm flatten --source-file src/proxy/truth/BSCLightClientProxy.sol         --json-file out/dapp.sol.json > flat/BSCLightClientProxy.f.sol
+hevm flatten --source-file src/proxy/truth/BSCLightClientProxy.sol         --json-file out/dapp.sol.json > flat/BSCLightClientProxy.f.sol
 hevm flatten --source-file src/proxy/truth/ChainMessageCommitterProxy.sol  --json-file out/dapp.sol.json > flat/ChainMessageCommitterProxy.f.sol
 hevm flatten --source-file src/proxy/truth/DarwiniaLightClientProxy.sol    --json-file out/dapp.sol.json > flat/DarwiniaLightClientProxy.f.sol
 hevm flatten --source-file src/proxy/truth/EthereumExecutionLayerProxy.sol --json-file out/dapp.sol.json > flat/EthereumExecutionLayerProxy.f.sol
 hevm flatten --source-file src/proxy/BridgeProxyAdmin.sol                  --json-file out/dapp.sol.json > flat/BridgeProxyAdmin.f.sol
-# hevm flatten --source-file src/truth/bsc/BSCLightClient.sol                --json-file out/dapp.sol.json > flat/BSCLightClient.f.sol
-# hevm flatten --source-file src/truth/bsc/BSCStorageVerifier.sol            --json-file out/dapp.sol.json > flat/BSCStorageVerifier.f.sol
+hevm flatten --source-file src/truth/bsc/BSCLightClient.sol                --json-file out/dapp.sol.json > flat/BSCLightClient.f.sol
+hevm flatten --source-file src/truth/bsc/BSCStorageVerifier.sol            --json-file out/dapp.sol.json > flat/BSCStorageVerifier.f.sol
 hevm flatten --source-file src/truth/darwinia/ChainMessageCommitter.sol    --json-file out/dapp.sol.json > flat/ChainMessageCommitter.f.sol
 hevm flatten --source-file src/truth/darwinia/DarwiniaMessageVerifier.sol  --json-file out/dapp.sol.json > flat/DarwiniaMessageVerifier.f.sol
 hevm flatten --source-file src/truth/darwinia/LaneMessageCommitter.sol     --json-file out/dapp.sol.json > flat/LaneMessageCommitter.f.sol
