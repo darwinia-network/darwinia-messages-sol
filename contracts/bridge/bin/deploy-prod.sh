52a05fe737fed8074688e3e06f3163c47b9de6a5
echo <hujw77@gmail.com>
2022-09-26 14:20:07 +0800

    update mainnet beacon light client bootstrap

--- contracts/bridge/bin/deploy-prod.sh
+++ contracts/bridge/bin/deploy-prod.sh
@@ -8,7 +8,7 @@ if [[ -z ${INFURA_KEY} ]]; then
 	echo "INFURA_KEY not found, please set it and re-run the last command."
 	exit 1
 fi
-export ETH_GAS_PRICE=2000000000
+export ETH_GAS_PRICE=1300000000
 . $(dirname $0)/deploy/prod/darwinia.sh
 # export ETH_GAS_PRICE=2000000000
 # . $(dirname $0)/deploy/prod/ethlive.sh
