98181e943e1ecc8abbc166e32faa0b95aff50252
echo <hujw77@gmail.com>
2022-11-14 16:34:59 +0800

    add coverage script

--- contracts/bridge/bin/coverage.sh
+++ contracts/bridge/bin/coverage.sh
@@ -4,7 +4,7 @@ set -e
 MAX_UNCOVERED=0 # Maximum number of uncovered lines allowed
 
 echo "Running coverage..."
-uncovered=$(dapp test -v --coverage | grep "\[31m" | wc -l)
+uncovered=$(dapp test -v --coverage --cov-match "src\/spec" | grep "\[31m" | wc -l)
 echo "Uncovered lines: $uncovered"
 
 if [[ $uncovered -gt $MAX_UNCOVERED ]]; then
