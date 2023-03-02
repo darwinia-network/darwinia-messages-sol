c8c7cb8db545b543c603884eabe6950f015ba437
echo <hujw77@gmail.com>
2023-03-02 11:17:45 +0800

    deploy

--- contracts/bridge/bin/deploy/test/deploy.sh
+++ contracts/bridge/bin/deploy/test/deploy.sh
@@ -3,19 +3,20 @@
 set -e
 
 # 0
-# export ETH_GAS_PRICE=10000000000
-# . $(dirname $0)/deploy/test/pangoro.sh
-# export ETH_GAS_PRICE=2000000000
-# . $(dirname $0)/deploy/test/goerli.sh
+export ETH_GAS_PRICE=10000000000
+. $(dirname $0)/deploy/test/pangoro0.sh
+. $(dirname $0)/deploy/test/pangoro1.sh
+export ETH_GAS_PRICE=2000000000
+. $(dirname $0)/deploy/test/goerli.sh
 # export ETH_GAS_PRICE=10000000000
 # . $(dirname $0)/deploy/test/bsctest.sh
 
 # 10
-export ETH_GAS_PRICE=10000000000
-. $(dirname $0)/deploy/test/pangoro-10.sh
-export ETH_GAS_PRICE=2000000000
-. $(dirname $0)/deploy/test/goerli-10.sh
-export ETH_GAS_PRICE=10000000000
-. $(dirname $0)/deploy/test/pangoro-11.sh
-export ETH_GAS_PRICE=2000000000
-. $(dirname $0)/deploy/test/goerli-11.sh
+# export ETH_GAS_PRICE=10000000000
+# . $(dirname $0)/deploy/test/pangoro-10.sh
+# export ETH_GAS_PRICE=2000000000
+# . $(dirname $0)/deploy/test/goerli-10.sh
+# export ETH_GAS_PRICE=10000000000
+# . $(dirname $0)/deploy/test/pangoro-11.sh
+# export ETH_GAS_PRICE=2000000000
+# . $(dirname $0)/deploy/test/goerli-11.sh
