0dc9752a5440cf55e235528097bbf8f12fd2d5a5
echo <hujw77@gmail.com>
2023-03-15 18:52:36 +0800

    test

--- contracts/bridge/e2e/helper/eip712test.js
+++ contracts/bridge/e2e/helper/eip712test.js
@@ -1,8 +1,8 @@
 const { signHash } = require('./eip712')
 
 message = {
-  "block_number": 10580,
-  "message_root": "0x56223be3175f9a12197c701ea64c9d7ae73d686fdf6129c85840a592e5e66d12",
+  "block_number": 11170,
+  "message_root": "0xf2479cdacab42936aa272f26caf69b08891232cb480d44704c9007ead04edea4",
   "nonce": 0
 }
 
