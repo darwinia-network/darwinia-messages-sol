ed268ecacfcb530d8515748870494f3e0c833317
Aki Wu <wuminzhe@gmail.com>
2023-03-16 18:14:30 +0800

    format

--- contracts/periphery/contracts/ethereum/EthereumEndpoint.sol
+++ contracts/periphery/contracts/ethereum/EthereumEndpoint.sol
@@ -6,10 +6,13 @@ import "../interfaces/IOutboundLane.sol";
 import "../interfaces/IFeeMarket.sol";
 
 contract EthereumEndpoint {
-    address public constant OUTBOUND_LANE = 0x5a10ca57e07133AA5132eF29BA1EBf0096a302B0;
-    address public constant FEE_MARKET = 0xA10D0C6e04845A5e998d1936249A30563c553417;
+    address public constant TO_DARWINIA_OUTBOUND_LANE =
+        0x5a10ca57e07133AA5132eF29BA1EBf0096a302B0;
+    address public constant TO_DARWINIA_FEE_MARKET =
+        0xA10D0C6e04845A5e998d1936249A30563c553417;
 
-    address public constant DARWINIA_ENDPOINT = 0x5a10ca57e07133AA5132eF29BA1EBf0096a302B0;
+    address public constant DARWINIA_ENDPOINT =
+        0x5a10ca57e07133AA5132eF29BA1EBf0096a302B0;
 
     // Ethereum > Darwinia > Parachain
     // dispatch a call on parachain
@@ -23,22 +26,25 @@ contract EthereumEndpoint {
         bytes2 paraId,
         bytes memory paraCall,
         uint64 weight
-    ) payable external returns (uint64 nonce) {
-        return executeOnDarwinia(
-            abi.encodeWithSignature(
-                "dispatchOnParachain(bytes2,bytes,uint64)", 
-                paraId,
-                paraCall,
-                weight
-            )
-        );
+    ) external payable returns (uint64 nonce) {
+        return
+            executeOnDarwinia(
+                abi.encodeWithSignature(
+                    "dispatchOnParachain(bytes2,bytes,uint64)",
+                    paraId,
+                    paraCall,
+                    weight
+                )
+            );
     }
 
-    // Ethereum > Darwinia 
-    function executeOnDarwinia(bytes memory call) payable public returns (uint64 nonce) {
-        return IOutboundLane(OUTBOUND_LANE).send_message{value: IFeeMarket(FEE_MARKET).market_fee()}(
-            DARWINIA_ENDPOINT,
-            call 
-        );
+    // Ethereum > Darwinia
+    function executeOnDarwinia(
+        bytes memory call
+    ) public payable returns (uint64 nonce) {
+        return
+            IOutboundLane(TO_DARWINIA_OUTBOUND_LANE).send_message{
+                value: IFeeMarket(TO_DARWINIA_FEE_MARKET).market_fee()
+            }(DARWINIA_ENDPOINT, call);
     }
 }
