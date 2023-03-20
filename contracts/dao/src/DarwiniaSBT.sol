0c3c26509239c227c416de890add54d49bc26a03
echo <hujw77@gmail.com>
2023-03-20 15:40:42 +0800

    make name and symbol changeable

--- contracts/dao/src/DarwiniaSBT.sol
+++ contracts/dao/src/DarwiniaSBT.sol
@@ -2,15 +2,20 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts@4.8.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts@4.8.2/access/Ownable.sol";
-import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@4.8.2/utils/Counters.sol";
 
 /// @custom:security-contact security@darwinia.network
-contract DarwiniaSBT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC721Burnable {
+/// Spec:
+/// 1. 
+contract DarwiniaSBT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
+
+    string private _name;
+    string private _symbol;
     Counters.Counter private _tokenIdCounter;
 
     // --- Auth ---
@@ -35,6 +40,14 @@ contract DarwiniaSBT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC
 
     function setTokenURI(uint256 tokenId, string memory uri) public onlyOwner {
         _setTokenURI(tokenId, uri);
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
     }
 
     function setName(string calldata newName) external onlyOwner {
