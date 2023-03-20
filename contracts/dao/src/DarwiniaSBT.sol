ca18575e29e37a53604ac305572b4f59da7de0e9
echo <hujw77@gmail.com>
2023-03-20 14:34:22 +0800

    darwinia dao profile

---
+++ contracts/dao/src/DarwiniaSBT.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/// @custom:security-contact security@darwinia.network
+contract DarwiniaSBT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC721Burnable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address guy) external onlyOwner { wards[guy] = 1; }
+    function deny(address guy) external onlyOwner { wards[guy] = 0; }
+    modifier auth {
+        require(wards[msg.sender] == 1, "DDP/not-authorized");
+        _;
+    }
+
+    constructor(address dao) ERC721("Darwinia DAO Profile", "DDP") {}
+
+    function safeMint(address to, string memory uri) public auth {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    // only contract owner could transfer/mint SBT
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view override returns (bool) {
+        return spender == owner();
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ contracts/dao/src/IERC721S.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Inspired from OpenZeppelin Contracts (v4.8.0) (token/ERC721/IERC721.sol)
+
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol';
+
+/**
+ * @dev Interface of an ERC721S compliant contract.
+ */
+interface IERC721S is IERC721, IERC721Metadata {}
