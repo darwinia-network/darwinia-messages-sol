2e40585871cae078856db625ac7c0596901e4cf5
Aki Wu <wuminzhe@gmail.com>
2023-03-22 16:51:04 +0800

    better example script

--- contracts/periphery/scripts/s2s-example/deploy-and-run.js
+++ contracts/periphery/scripts/s2s-example/deploy-and-run.js
@@ -1,4 +1,3 @@
-process.env.HARDHAT_NETWORK = "pangolin";
 const hre = require("hardhat");
 
 // Deploy, then, 
@@ -7,13 +6,33 @@ async function main() {
     ////////////////////////////////////////
     // Deploy
     ////////////////////////////////////////
-    // Pangolin
+
+    // PANGORO
     // -------------
+    hre.changeNetwork("pangoro");
+    // Deploy endpoint on Pangoro
+    const PangoroEndpoint = await hre.ethers.getContractFactory("PangoroEndpoint");
+    const pangoroEndpoint = await PangoroEndpoint.deploy();
+    await pangoroEndpoint.deployed();
+    console.log(
+        `Pangoro Endpoint: ${pangoroEndpoint.address}`
+    );
+
+    // Deploy `Callee` on Pangoro
+    const Callee = await hre.ethers.getContractFactory("Callee");
+    const callee = await Callee.deploy();
+    await callee.deployed();
+    console.log(
+        `Callee: ${callee.address}`
+    );
+
+    // PANGOLIN
+    // -------------
+    hre.changeNetwork("pangolin");
     // Deploy endpoint on Pangolin
     const PangolinEndpoint = await hre.ethers.getContractFactory("PangolinEndpoint");
     const pangolinEndpoint = await PangolinEndpoint.deploy();
     await pangolinEndpoint.deployed();
-
     console.log(
         `Pangolin Endpoint: ${pangolinEndpoint.address}`
     );
@@ -22,49 +41,25 @@ async function main() {
     const Caller = await hre.ethers.getContractFactory("Caller");
     const caller = await Caller.deploy(pangolinEndpoint.address);
     await caller.deployed();
-
     console.log(
         `Caller: ${caller.address}`
     );
 
-    // Pangoro
-    // -------------
-    hre.changeNetwork("pangoro");
-    // Deploy endpoint on Pangoro
-    const PangoroEndpoint = await hre.ethers.getContractFactory("PangoroEndpoint");
-    const pangoroEndpoint = await PangoroEndpoint.deploy();
-    await pangoroEndpoint.deployed();
-
-    console.log(
-        `Pangoro Endpoint: ${pangoroEndpoint.address}`
-    );
-
-    // // Optional for this example
-    // const PANGOLIN_CHAIN_ID = 0x7061676c; // pagl
-    // await (await pangolinEndpoint.setRemoteEndpoint(PANGOLIN_CHAIN_ID, pangolinEndpoint.address)).wait();
-
-    // Deploy `Callee` on Pangoro
-    const Callee = await hre.ethers.getContractFactory("Callee");
-    const callee = await Callee.deploy();
-    await callee.deployed();
-
-    console.log(
-        `Callee: ${callee.address}`
-    );
-
-    // Pangolin
-    // -------------
     // Let Pangolin endpoint know the Pangoro endpoint
     const PANGORO_CHAIN_ID = 0x70616772; // pagr
     await (await pangolinEndpoint.setRemoteEndpoint(PANGORO_CHAIN_ID, pangoroEndpoint.address)).wait();
+    console.log(
+        `PangolinEndpoint knowns PangoroEndpoint.`
+    );
 
     ////////////////////////////////////////
     // Remote call
     ////////////////////////////////////////
+    const fee = await pangolinEndpoint.fee();
     const tx = await caller.remoteAdd(
         callee.address,
         {
-            value: hre.ethers.utils.parseEther("0.1")
+            value: fee
         }
     )
 
