0cf608b4d08e4114dc8a8cce1ffbf1fad7afc45c
echo <hujw77@gmail.com>
2023-04-13 10:06:30 +0800

    deploy and verify on crab

---
+++ contracts/other/root/flat/Root.f.sol
@@ -0,0 +1,211 @@
+// hevm: flattened sources of src/Root.sol
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17 >=0.8.0 <0.9.0;
+
+////// lib/zeppelin-solidity/contracts/utils/Context.sol
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+/* pragma solidity ^0.8.0; */
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+////// lib/zeppelin-solidity/contracts/access/Ownable.sol
+// OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
+
+/* pragma solidity ^0.8.0; */
+
+/* import "../utils/Context.sol"; */
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+////// lib/zeppelin-solidity/contracts/access/Ownable2Step.sol
+// OpenZeppelin Contracts (last updated v4.8.0) (access/Ownable2Step.sol)
+
+/* pragma solidity ^0.8.0; */
+
+/* import "./Ownable.sol"; */
+
+/**
+ * @dev Contract module which provides access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership} and {acceptOwnership}.
+ *
+ * This module is used through inheritance. It will make available all functions
+ * from parent (Ownable).
+ */
+abstract contract Ownable2Step is Ownable {
+    address private _pendingOwner;
+
+    event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Returns the address of the pending owner.
+     */
+    function pendingOwner() public view virtual returns (address) {
+        return _pendingOwner;
+    }
+
+    /**
+     * @dev Starts the ownership transfer of the contract to a new account. Replaces the pending transfer if there is one.
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual override onlyOwner {
+        _pendingOwner = newOwner;
+        emit OwnershipTransferStarted(owner(), newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`) and deletes any pending owner.
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual override {
+        delete _pendingOwner;
+        super._transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev The new owner accepts the ownership transfer.
+     */
+    function acceptOwnership() external {
+        address sender = _msgSender();
+        require(pendingOwner() == sender, "Ownable2Step: caller is not the new owner");
+        _transferOwnership(sender);
+    }
+}
+
+////// src/Root.sol
+/* pragma solidity 0.8.17; */
+
+/* import "@openzeppelin/contracts@4.8.2/access/Ownable2Step.sol"; */
+
+contract Root is Ownable2Step {
+
+    // @dev dispatch precompile address(0x0401)
+    address public immutable DISPATCH_PRECOMPILE;
+
+    constructor(address sudo, address dispatchPrecompile) {
+        _transferOwnership(sudo);
+        DISPATCH_PRECOMPILE = dispatchPrecompile;
+    }
+
+    function dispatch(bytes calldata data) external payable returns (bytes memory) {
+        return execute(DISPATCH_PRECOMPILE, data);
+    }
+
+    function execute(address target, bytes calldata data) public payable onlyOwner returns (bytes memory) {
+        (bool ok, bytes memory out) = target.call{value: msg.value}(data);
+        if (ok) {
+            return out;
+        } else {
+            _revert(out, "!call");
+        }
+    }
+
+    function _revert(bytes memory returndata, string memory errorMessage) private pure {
+        if (returndata.length > 0) {
+            assembly ("memory-safe") {
+                let returndata_size := mload(returndata)
+                revert(add(32, returndata), returndata_size)
+            }
+        } else {
+            revert(errorMessage);
+        }
+    }
+}
+
