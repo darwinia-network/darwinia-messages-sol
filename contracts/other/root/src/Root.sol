a14d7dfbc8e9970e800308bab13a0544c97d9308
echo <hujw77@gmail.com>
2023-04-13 13:25:17 +0800

    Root contract (#319)

---
+++ contracts/other/root/src/Root.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts@4.8.2/access/Ownable2Step.sol";
+
+contract Root is Ownable2Step {
+
+    // @dev dispatch precompile address(0x0401)
+    address public immutable DISPATCH_PRECOMPILE;
+
+    constructor(address sudo, address dispatchPrecompile) {
+        _transferOwnership(sudo);
+        DISPATCH_PRECOMPILE = dispatchPrecompile;
+    }
+
+    function dispatch(bytes calldata data) external payable returns (bytes memory) {
+        return execute(DISPATCH_PRECOMPILE, data);
+    }
+
+    function execute(address target, bytes calldata data) public payable onlyOwner returns (bytes memory) {
+        (bool ok, bytes memory out) = target.call{value: msg.value}(data);
+        if (ok) {
+            return out;
+        } else {
+            _revert(out, "!call");
+        }
+    }
+
+    function _revert(bytes memory returndata, string memory errorMessage) private pure {
+        if (returndata.length > 0) {
+            assembly ("memory-safe") {
+                let returndata_size := mload(returndata)
+                revert(add(32, returndata), returndata_size)
+            }
+        } else {
+            revert(errorMessage);
+        }
+    }
+}
